7db78f4a2711100b128b55554f2c184f89c626c5
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2021-09-23 20:56:28 +0600

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install',event => {
+     event.waitUntil(caches.open('static').then(cache => {
+          return cache.addAll(["./","./source/design.css","./images/logo192.png"]);
+     }));
+});
+self.addEventListener('fetch',event => {
+     event.respondWith(caches.match(event.request).then(response => {
+          return response || fetch(event.request);
+     }));
+});
