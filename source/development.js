92e71499f7aa8ef52dc3c3b27cfaddb7dcc68a69
Minhaz Halim (Zim) <31536403+minhazhalim@users.noreply.github.com>
2021-09-23 20:54:17 +0600

    Add files via upload

---
+++ source/development.js
@@ -0,0 +1,9 @@
+if('serviceWorker' in navigator){
+     navigator.serviceWorker.register('serviceworker.js').then(registration => {
+          console.log('ServiceWorker Registered');
+          console.log(registration);
+     }).catch(error => {
+          console.log('ServiceWorker Registration Failed!');
+          console.log(error);
+     });
+}
